d376c02e3c66a045b7dbdefd91e847064176689e
biplan <bannikovilea@gmail.com>
2020-02-03 13:58:54 +0500

    Some stupid implementation of JSON5 for golang

--- decode.go
+++
@@ -1,9 +0,0 @@
-package json5
-
-func Unmarshal(data []byte, v interface{}) error {
-	return nil
-}
-
-type Unmarshaler interface {
-	UnmarshalJSON5([]byte) error
-}
--- encode.go
+++
@@ -1,9 +0,0 @@
-package json5
-
-func Marshal(v interface{}) ([]byte, error) {
-	return nil, nil
-}
-
-type Marshaler interface {
-	MarshalJSON5([]byte, error)
-}
